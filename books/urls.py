6a72fdd8eda1af42a6872b9006e8847de78de594
Your Correct Name <otajonbozorboyev571@gmail.com>
2024-10-29 18:35:13 +0500

    library_api

---
+++ books/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    BookListApiView, 
+    BookDetailApiView, 
+    BookDeleteApiView, 
+    BookUpdateApiView, 
+    BookCreateApiView
+)
+
+urlpatterns = [
+    path('books/', BookListApiView.as_view(), ),
+    path('books/create/', BookCreateApiView.as_view()),
+    path('books/<int:pk>/', BookDetailApiView.as_view()),
+    path('books/<int:pk>/update/', BookUpdateApiView.as_view()),
+    path('books/<int:pk>/delete/', BookDeleteApiView.as_view()),
+]
---
+++ books/views.py
@@ -0,0 +1,37 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
+
+
+class BookListApiView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailApiView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteApiView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateApiView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateApiView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+@api_view(['GET'])
+def book_list_view(request, *args, **kwargs):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
