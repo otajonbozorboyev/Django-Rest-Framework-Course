8da3abda154419f01435b77d469912a3e79d4323
Your Correct Name <otajonbozorboyev571@gmail.com>
2024-12-19 12:07:21 +0500

    BlogAPP Full Project API

---
+++ blogapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ blogapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer, BlogSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserRegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def create_blog(request):
+    serializer = BlogSerializer()
+
+
+
